c76ee2fe97a91ba14f8de14e3f76b0e0817f271d
GYTumlisan <gymoa2011@gmail.com>
2023-04-05 21:39:54 -0700

    Initial commit repository2

---
+++ src/hw_demo/HWClass2.java
@@ -0,0 +1,11 @@
+package hw_demo;
+
+public class HWClass2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Local repository 2....");
+		
+		System.out.println("Remote repository 2....");
+	}
+}
